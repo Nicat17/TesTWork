07bbad94237a0d616ce01a962abd5563cbab62f6
Nicat17 <nijatsa@code.edu.az>
2019-11-08 16:08:28 +0400

    Initialize

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    interface IBook<out T>
+    {
+        T Read();
+    }
+    interface INoteBook<in T>
+    {
+        void Write<T>(T arg) where T : class, new();
+    }
+
+    class Book<T> : IBook<T>
+    {
+        public T Read()
+        {
+            T obj = (T)Activator.CreateInstance(typeof(T));
+            return obj;
+        }
+    }
+
+
+
+    class NewsPaper<T> : INoteBook<T>
+    {
+        public void Write<T>(T arg) where T : class, new()
+        {
+            
+        }
+    }
+
+    class CopyBook<T> : INoteBook<T>
+    {
+        public void Write<T>(T arg) where T :class,new()
+        {
+            
+        }
+    }
+
+    class Paper
+    {
+
+    }
+    class Sheet : Paper
+    {
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INoteBook<Sheet> noteBook = new CopyBook<Paper>();
+
+            Sheet sheet = (new Paper()) as Sheet;
+
+            Console.ReadLine();
+        }
+    }
+}
