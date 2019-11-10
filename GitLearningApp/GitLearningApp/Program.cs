d16a7bf497157befd0e734a953a534512fbcb9c8
AbdullayevNicat <nijattttsa@gmail.com>
2019-11-10 21:12:05 +0400

    Some file is deleted and added

---
+++ GitLearningApp/GitLearningApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GitLearningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Thread thread = new System.Threading
+                .Thread(new System.Threading.ParameterizedThreadStart(delegate(object x) 
+            {
+                string currentDir = System.IO.Directory.GetCurrentDirectory();
+
+                string fileName = "example.txt";
+
+                string fullName = System.IO.Path.Combine(currentDir,fileName);
+
+                if (!System.IO.File.Exists(fullName))
+                {
+                    System.IO.File.Create(fullName);
+                }
+                else
+                    System.Console.WriteLine("Already has file");
+            }));
+
+            thread.Start();
+
+            System.Console.ReadLine();
+        }
+    }
+}
